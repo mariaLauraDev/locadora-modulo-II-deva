fa9c999bdd35e92d0e6f56ac095c98e02aa9c820
Maria Laura <marialauradev@gmail.com>
2022-10-27 20:57:35 -0300

    initial commit

---
+++ src/Cd.java
@@ -0,0 +1,40 @@
+public class Cd extends Midia {
+
+    private String artista;
+    private int quantidadedeFaixas;
+
+    public Cd(String codigo,
+              Double preco,
+              String nome,
+              String artista,
+              int quantidadedeFaixas) {
+        super(codigo, preco, nome);
+        this.artista = artista;
+        this.quantidadedeFaixas = quantidadedeFaixas;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public int getQuantidadedeFaixas() {
+        return quantidadedeFaixas;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public void setQuantidadedeFaixas(int quantidadedeFaixas) {
+        this.quantidadedeFaixas = quantidadedeFaixas;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Cd{" +
+                "artista='" + artista + '\'' +
+                ", quantidadedeFaixas=" + quantidadedeFaixas +
+                '}';
+    }
+}
---
+++ src/Midia.java
@@ -0,0 +1,50 @@
+public class Midia {
+    private String codigo;
+    private Double preco;
+    private String nome;
+
+    public Midia(String codigo,
+                 Double preco,
+                 String nome) {
+        this.codigo = codigo;
+        this.preco = preco;
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public static void darPlay(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "Midia{" +
+                "codigo='" + codigo + '\'' +
+                ", preco=" + preco +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+}
