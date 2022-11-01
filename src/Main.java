8aba3e62f8fb6bc47b46ac31a253708be117ba53
Jessica Maciel <jessicateixeiramaciel@gmail.com>
2022-10-31 23:09:17 -0300

    Configuração da classe DoacaoMidia.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Dvd starWars = new Dvd("DVD-001",19.90,"Star Wars","ingles",true,"portugues");
-        Dvd madMax = new Dvd("DVD-002",15.90,"Mad Max","ingles",true,"portugues");
+        Dvd starWars = new Dvd("DVD-001",19.90,"Star Wars","inglês",true,"portugues");
+        Dvd madMax = new Dvd("DVD-002",15.90,"Mad Max","inglês",true,"inglês");
         Dvd oParasita = new Dvd("DVD-003",25.90,"O Parasita","coreano",true);
 
         DoacaoMidia.assistirFilme(starWars);
