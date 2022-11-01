cbd3528f7c000aaf9c79c6a578036aefa3e5d45d
Maria Laura <marialauradev@gmail.com>
2022-10-31 23:38:25 -0300

    Correção de Cds

--- src/DoacaoMidia.java
+++ src/DoacaoMidia.java
@@ -7,6 +7,9 @@ public class DoacaoMidia {
         Dvd starWars = new Dvd("DVD-001",5.90,"A Lagoa Azul","inglês",true,"português");
         Dvd madMax = new Dvd("DVD-002",5.90,"Velozes e Furiosos","inglês",true,"português");
         Dvd oParasita = new Dvd("DVD-003",5.90,"Top Gun","coreano",true);
+        Cd oneDirection = new Cd("CD-001",25.5,"One Direction", "Louis, Zayn, Nail, Harry",12);
+        Cd justinBieber = new Cd("CD-002",30.0,"Baby", "Justin Bieber",15);
+        Cd blackPink = new Cd("CD-003",50.0,"Map of the soul", "Back Pink",20);
 
         Random gerador = new Random();
 
@@ -16,6 +19,9 @@ public class DoacaoMidia {
             case 0 : return starWars;
             case 1 : return madMax;
             case 2 : return oParasita;
+            case 3: return oneDirection;
+            case 4: return justinBieber;
+            case 5: return blackPink;
             default : return starWars;
         }
     }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Dvd starWars = new Dvd("DVD-001",19.90,"Star Wars","inglês",true,"portugues");
-        Dvd madMax = new Dvd("DVD-002",15.90,"Mad Max","inglês",true,"inglês");
-        Dvd oParasita = new Dvd("DVD-003",25.90,"O Parasita","coreano",true);
 
         System.out.println("------------------ MÍDIAS PARA DOAÇÃO ------------------");
 
-        System.out.println("O filme " + DoacaoMidia.pegarMidiaAleatoria().getNome() + " foi selecionado para doação.");
+        System.out.println("A mídia " + DoacaoMidia.pegarMidiaAleatoria().getNome() + " foi selecionado para doação.");
     }
 }
