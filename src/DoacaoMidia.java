34f40654595ad2a8d53e93f5fccf42b35ec24b0a
Jessica Maciel <jessicateixeiramaciel@gmail.com>
2022-10-31 23:02:33 -0300

    Configuração da classe DoacaoMidia.

--- src/DoacaoMidia.java
+++ src/DoacaoMidia.java
@@ -2,16 +2,27 @@ import java.util.Random;
 
 
 public class DoacaoMidia {
-    static String[] opcoes = { "DVD-001", "DVD-002", "DVD-003"};
 
-    public static void pegarMidiaAleatoria(){
-        String midiaAleatoria = opcoes[new Random().nextInt(opcoes.length)];
-        System.out.println("A mídia de código " + midiaAleatoria + " foi selecionada para doação.");
+    public static Midia pegarMidiaAleatoria(){
+        Dvd starWars = new Dvd("DVD-001",5.90,"A Lagoa Azul","inglês",true,"português");
+        Dvd madMax = new Dvd("DVD-002",5.90,"Velozes e Furiosos","inglês",true,"português");
+        Dvd oParasita = new Dvd("DVD-003",5.90,"Top Gun","coreano",true);
+
+        Random gerador = new Random();
+
+        int valorAleatorio = gerador.nextInt(4);
+
+        switch (valorAleatorio){
+            case 0 : return starWars;
+            case 1 : return madMax;
+            case 2 : return oParasita;
+            default : return starWars;
+        }
     }
 
     public static void assistirFilme(Dvd dvd){
         dvd.darPlay();
         dvd.legenda(true);
-        dvd.legenda(true,"portugues");
+        dvd.legenda(true,"português");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         Dvd starWars = new Dvd("DVD-001",19.90,"Star Wars","ingles",true,"portugues");
-        Dvd madMax = new Dvd("DVD-002",12.90,"Mad Max","ingles",true,"portugues");
-        Dvd oParasita = new Dvd("DVD-003",15.90,"O Parasita","coreano",true);
+        Dvd madMax = new Dvd("DVD-002",15.90,"Mad Max","ingles",true,"portugues");
+        Dvd oParasita = new Dvd("DVD-003",25.90,"O Parasita","coreano",true);
 
         DoacaoMidia.assistirFilme(starWars);
-        DoacaoMidia.pegarMidiaAleatoria();
+        System.out.println("O filme " + DoacaoMidia.pegarMidiaAleatoria().getNome() + " foi selecionado para doação.");
     }
 }
